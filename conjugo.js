764c168d3da72674504ed8ee13e756216a661dfd
David Kirkby <dkirkby@uci.edu>
2018-07-11 09:35:19 +0200

    WIP

--- conjugo.js
+++ conjugo.js
@@ -7,6 +7,19 @@ $(window).on( "load", function() {
     console.log("window loaded");
 });
 
+var nverb = 0;
+var pron = null;
+var conj = null;
+var regular = null;
+
+function initData(cdata) {
+    pron = cdata["pronouns"];
+    conj = cdata["conjugations"];
+    regular = cdata["regular"];
+    nverb = Object.keys(conj).length;
+    console.log("loaded " + nverb + " verbs.");
+}
+
 function initialize(jQuery) {
     // Implement toggling tenses on/off.
     $(".tense > span").click(function() {
@@ -48,43 +61,63 @@ function choose(a) {
     return a[rint(a.length)];
 }
 
+var queryTense = null;
+var queryAnswer = null;
+
 function next() {
     // Pick a random verb.
-    vidx = rint(nverb);
-    verb = conj[vidx];
-    info = verb['info'];
-    conjs = verb['conjs'];
-    $("#verb-input").attr("placeholder", info["name"]);
+    var vidx = rint(nverb);
+    var verb = conj[vidx];
+    var info = verb["info"];
+    var name = info["name"];
+    var ending = name.slice(-2);
+    if(ending == "se") {
+        ending = name.slice(-4);
+    }
+    console.log("ending", ending);
+    $("#verb-input").attr("placeholder", name);
     $("#translation").text(info["en"]);
     // Pick a random pronoun.
-    plist = choose(pron);
+    var pidx = rint(pron.length);
+    var plist = pron[pidx];
     p = choose(plist.split("/"));
     console.log('verb', vidx, p, info['name']);
     $("#pronoun").text(p);
     // Pick an active tense.
     var active = $(".tense > span.active");
-    var tense = $(choose(active));
-    var tidx = tense.data("tense");
-    console.log("tense", tidx, tense.text());
-}
-
-var nverb = 0;
-var pron = null;
-var conj = null;
-var regular = null;
-
-function initData(cdata) {
-    pron = cdata["pronouns"];
-    conj = cdata["conjugations"];
-    regular = cdata["regular"];
-    nverb = Object.keys(conj).length;
-    console.log("loaded " + nverb + " verbs.");
+    queryTense = $(choose(active));
+    queryTense.addClass("query");
+    var tidx = queryTense.data("tense");
+    console.log("tense", tidx, queryTense.text());
+    // Remember the correct answer.
+    queryAnswer = verb['conjs'];
+    if(queryAnswer == "$") {
+        // All tenses of this verb are regular.
+        queryAnswer = regular[ending][tidx][pidx];
+    }
+    else {
+        queryAnswer = queryAnswer[tidx];
+        if(queryAnswer == "*") {
+            // This tense is regular.
+            queryAnswer = regular[ending][tidx][pidx];
+        }
+        else {
+            queryAnswer = queryAnswer[pidx];
+            if(queryAnswer == "*") {
+                // This pronoun is regular.
+                queryAnswer = regular[ending][tidx][pidx];
+            }
+        }
+    }
+    console.log("answer", queryAnswer);
 }
 
 function handleAnswer() {
     var solution = $("#verb-input").val();
     if(solution != '') {
+        $("#answer2").text(queryAnswer);
         $("#answer1").text(solution);
         $("#verb-input").val('');
     }
+    queryTense.removeClass("query");
 }
